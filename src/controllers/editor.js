8b79798564bc4903ceb23252cd960c153df16f08
brandon <brandon@rsonservice.com>
2024-04-05 09:13:32 -0400

    fix: fixed some error

--- src/controllers/editor.js
+++ src/controllers/editor.js
@@ -2,11 +2,10 @@ const { spawn, exec } = require("child_process");
 const { uploader } = require("../utils/uploader");
 
 const processBgFg = async (req, res) => {
-  console.log("ffmpeg " + req.body.command.join(" "));
-  stream = spawn("ffmpeg", req.body.command);
-
-  stream.stdout.on("data", (data) => {
-    console.log(`stdout: ${data}`);
+  const uploadResult = await uploader("videoshot.mp4");
+  res.send({
+    statusCode: 200,
+    message: uploadResult,
   });
 
   stream.stderr.on("data", (data) => {
